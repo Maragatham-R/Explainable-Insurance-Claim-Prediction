54789a44ba55624eeacde6d17e02442c36290dee
Maragatham-R <maragatham4563@gmail.com>
2026-01-19 12:42:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,11 +99,11 @@ if st.button("🔍 Predict Claim"):
 
     shap_values = explainer.shap_values(user_input)
 
-   fig = shap.force_plot(
-        explainer.expected_value[1],
-        shap_values[1],
-        user_input,
-        matplotlib=True
+    fig = shap.force_plot(
+         explainer.expected_value[1],
+         shap_values[1],
+         user_input,
+         matplotlib=True
     )
     st.pyplot(fig, bbox_inches="tight")
 
